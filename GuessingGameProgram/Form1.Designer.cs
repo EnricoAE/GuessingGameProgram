db363d8e57955473e81f646aaadf78d73a3ea673
EnricoAE <113055012+EnricoAE@users.noreply.github.com>
2023-02-10 23:00:57 +0800

    Initial commit

---
+++ GuessingGameProgram/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace GuessingGameProgram
+{
+    partial class frGuessingGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGuess = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblGuess
+            // 
+            this.lblGuess.AutoSize = true;
+            this.lblGuess.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.lblGuess.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGuess.ForeColor = System.Drawing.Color.White;
+            this.lblGuess.Location = new System.Drawing.Point(18, 60);
+            this.lblGuess.MinimumSize = new System.Drawing.Size(400, 100);
+            this.lblGuess.Name = "lblGuess";
+            this.lblGuess.Size = new System.Drawing.Size(400, 100);
+            this.lblGuess.TabIndex = 0;
+            this.lblGuess.Text = "(  ---  )";
+            this.lblGuess.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAnswer.Location = new System.Drawing.Point(107, 202);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(227, 32);
+            this.txtAnswer.TabIndex = 1;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.btnGuess.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuess.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.ForeColor = System.Drawing.Color.White;
+            this.btnGuess.Location = new System.Drawing.Point(121, 262);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(196, 48);
+            this.btnGuess.TabIndex = 2;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = false;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 23;
+            this.listBox1.Location = new System.Drawing.Point(459, 60);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(255, 280);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(459, 20);
+            this.label2.MinimumSize = new System.Drawing.Size(255, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(255, 30);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Wrong guess...";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(18, 20);
+            this.label1.MinimumSize = new System.Drawing.Size(400, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(400, 30);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Guess the word!";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // frGuessingGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 359);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.lblGuess);
+            this.Name = "frGuessingGame";
+            this.Text = "Guessing Game";
+            this.Load += new System.EventHandler(this.frGuessingGame_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGuess;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GuessingGameProgram/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace GuessingGameProgram
+{
+    public partial class frGuessingGame : Form
+    {
+        public frGuessingGame()
+        {
+            InitializeComponent();
+        }
+
+        private void frGuessingGame_Load(object sender, EventArgs e)
+        {
+            StringBuilder GuessComp = new StringBuilder("c??????r");
+            lblGuess.Text = GuessComp.ToString();
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            ArrayList ans = new ArrayList();
+            ans.Add("computer"); 
+
+            string attempt = txtAnswer.Text;
+
+            if(ans.Contains(attempt) && attempt.Equals("computer"))
+            {
+                lblGuess.Text = attempt.ToString();
+                txtAnswer.Text = "";
+                MessageBox.Show("Correct Guess!");
+            }
+            else
+            {
+                MessageBox.Show("Wrong guess! \nTry again");
+                listBox1.Items.Add(txtAnswer.Text);
+                txtAnswer.Text = "";
+            }
+        }
+    }
+}
